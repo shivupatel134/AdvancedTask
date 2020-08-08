d88c5f22bd20a8e3fd598c014129bcbfad185de9
Shivani Patel <Shivani Patel@DESKTOP-B130C8V>
2020-08-08 17:04:51 +1200

    initial commit

---
+++ ReactScripts/Profile/SocialMediaLinkedAccount.jsx
@@ -0,0 +1,39 @@
+﻿/* Social media JSX */
+import React from 'react';
+import { ChildSingleInput } from '../Form/SingleInput.jsx';
+import { Popup, Button } from 'semantic-ui-react';
+
+export default class SocialMediaLinkedAccount extends React.Component {
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    componentDidMount() {
+        $('.ui.button.social-media')
+            .popup();
+    }
+
+
+
+    render() {
+        return (<div>
+            
+                <button className="ui linkedin button">
+                    <i class="linkedin icon"></i>
+                    LinkedIn
+                </button>
+                <button class="ui github secondary button">
+                    <i class="github icon"></i>
+                    GitHub
+                </button>
+            <span className="right floated">
+            <button class="ui secondary  button">
+                    Edit
+                </button>
+                </span>
+        </div>)
+    }
+
+}
